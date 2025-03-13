98ac2674fada4bdc9bd905ea4c908b3d0c5c530a
ViniSouza001 <viniciussouza130705@gmail.com>
2025-03-13 15:31:51 -0300

    system of select and unselect cells

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const main = document.querySelector("main");
 let limit = 9;
 let numberCell = 0;
+const cells = [];
+let selectedCell;
 
 // Building cells
 const createElement = (name) => {
@@ -13,17 +15,33 @@ const buildSquad = () => {
     const squad = createElement("squad");
     main.appendChild(squad);
     buildCell(squad);
+}
 
+// function clicking on cells
+const handleClick = (element) => {
+    // select or unselect the same cell
+    if(element == selectedCell) {
+        element.classList.remove("selected");
+        selectedCell = undefined;
+    } else {
+        cells.forEach(c => c.classList.remove("selected"));
+        element.classList.toggle("selected");
+        selectedCell = element;
+    }
 }
 
 const buildCell = (squad) => {
     for(let i = 0; i < limit; i++) {
-        const cell = createElement(`cell "${numberCell}"`);
+        const cell = createElement(`cell ${numberCell}`);
         squad.appendChild(cell);
         numberCell = numberCell + 1;
+        cells.push(cell)
+
+        cell.onclick = () => {handleClick(cell)}
     }
 }
 
+// start of all functions
 for(let i = 0; i < limit; i++) {
     buildSquad();
 }
